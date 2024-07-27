3c461483f836ea6a71898a79677e7eb2076aec48
mandi7469 <mandi7469@aol.com>
2024-07-26 20:14:11 -0400

    destructured the items in req.body

--- routes/notes.js
+++ routes/notes.js
@@ -6,41 +6,40 @@ const uuid = require('../helpers/uuid');
 // Helper functions for reading and writing to the JSON file
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
-// GET Route for retrieving all the feedback
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for feedback`);
+  console.info(`${req.method} request received for notes`);
 
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for submitting feedback
+// POST Route for submitting a note
 notes.post('/', (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to submit feedback`);
+  console.info(`${req.method} request received to submit notes`);
 
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
-
+  
+  const { title, text } = req.body;
   // If all the required properties are present
-  if (email && feedbackType && feedback) {
+  if (title && text) {
     // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuid(),
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
     };
 
-    readAndAppend(newFeedback, './db/feedback.json');
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
       status: 'success',
-      body: newFeedback,
+      body: newNote,
     };
 
     res.json(response);
   } else {
-    res.json('Error in posting feedback');
+    res.json('Error in posting note');
   }
 });
 
--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/', (req, res) =>
 );
 
 // This view route is a GET route for the feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
